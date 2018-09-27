db9351843d67c8137ad0a6d43fd3f76ba774aee1
Oleh Raban (Zemelia) <o.raban@internetdevels.com>
2018-09-27 11:07:39 +0300

    Update sticky table with correct cells positioning, use second fake table element for sticky instead of second thead.

--- src/SimpleStickyTableHeader.js
+++ src/SimpleStickyTableHeader.js
@@ -14,55 +14,77 @@ function stickyTableHeader(table, scrollParent = document.body) {
   if (!thead) {
     return
   }
-  let sticked = false;
-  let stickyHeader = null;
-  let bodyFirstRowCells = [];
   const parentBox = scrollParent.getBoundingClientRect();
+  let resizeTimeout = null;
+  let sticked = false;
+  let stickyTable = null;
+
   function setWidth() {
     const headerCells = thead.getElementsByTagName('th');
-    const stickyHeaderCells =  stickyHeader.getElementsByTagName('th');
-    const tbody = table.getElementsByTagName('tbody')[0];
-    if (tbody) {
-      const firstRow = tbody.getElementsByTagName('tr')[0];
-      if (firstRow) {
-        bodyFirstRowCells = firstRow.getElementsByTagName('td');
-      }
-    }
+    const stickyHeaderCells =  stickyTable.getElementsByTagName('th');
+
     [].forEach.call(headerCells, (cell, key) => {
       stickyHeaderCells[key].style.width = cell.offsetWidth + 'px';
-      if (bodyFirstRowCells[key]) {
-        bodyFirstRowCells[key].style.width = cell.offsetWidth + 'px';
+      if (headerCells[key]) {
+        headerCells[key].style.width = cell.offsetWidth + 'px';
       }
     });
+    // Get border width as modifier to prevent misalignment with cells.
+    const computedStyles = window.getComputedStyle(headerCells[0]);
+    const widthModifier = parseInt(computedStyles.getPropertyValue('border-left-width'), 10);
+    stickyTable.style.width = table.offsetWidth + 'px';
+    stickyTable.getElementsByTagName('thead')[0].style.width = (thead.offsetWidth + widthModifier) + 'px';
   }
+
   function prepareHeader() {
     const offsetLeft = table.offsetLeft;
     const clonedThead = thead.cloneNode(true);
-    clonedThead.classList.add('sticky-header');
-    clonedThead.style.left = (offsetLeft + parentBox.left) + 'px';
-    clonedThead.style.top = parentBox.top + 'px';
-    clonedThead.style.position = 'fixed';
-    clonedThead.style.display = 'none';
-    table.insertBefore(clonedThead, table.childNodes[0]);
-    stickyHeader = clonedThead;
+    stickyTable = document.createElement('table');
+    stickyTable.style.left = (offsetLeft + parentBox.left) + 'px';
+    stickyTable.style.top = parentBox.top + 'px';
+    stickyTable.style.position = 'fixed';
+    stickyTable.style.overflow = 'hidden';
+    stickyTable.style.display = 'none';
+    stickyTable.classList.add('sticky-table');
+    clonedThead.style.display = 'block';
+    stickyTable.appendChild(clonedThead);
+    table.parentNode.insertBefore(stickyTable, table);
     setWidth();
   }
+
   function eventListener (e) {
     const offsetTop = table.offsetTop;
     let topStickOffset = offsetTop;
     let bottomStickOffset = offsetTop + table.offsetHeight - thead.offsetHeight;
-    if (sticked !== true && (offsetTop - scrollParent.scrollTop < 0)) {
+    if (offsetTop - scrollParent.scrollTop < 0 && bottomStickOffset > scrollParent.scrollTop) {
       sticked = true;
-      stickyHeader.style.display = null;
+      stickyTable.style.display = null;
     }
     if (sticked === true && (topStickOffset - scrollParent.scrollTop > 0 || bottomStickOffset < scrollParent.scrollTop)) {
       sticked = false;
-      stickyHeader.style.display = 'none';
+      stickyTable.style.display = 'none';
     }
   }
 
+  // Prepare header with base styles and html.
   prepareHeader();
+
+  // Start listen for parent scroll.
   scrollParent.addEventListener('scroll', eventListener);
+
+  // Table scroll event, to have same scrollLeft position for sticky table based on parent one.
+  table.addEventListener('scroll', (e) => {
+    stickyTable.scrollLeft = table.scrollLeft
+  });
+
+  // Resize with debounce to update widths.
+  function resizeDebounce() {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+      setWidth();
+    }, 200);
+  }
+  window.addEventListener('resize', resizeDebounce);
 }
 
 export default stickyTableHeader;
